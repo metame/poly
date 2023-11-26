fc79fa6cdc44f3a147abe30aaf750509afe98f98
Michael J Erwin <michaeljameserwin@gmail.com>
2023-11-25 21:57:32 -0500

    Poly: the tool to end all tools

---
+++ README.md
@@ -0,0 +1,23 @@
+# poly
+
+The tool to end all tools
+
+## Usage
+
+`$ poly <language> <command>`
+
+e.g.
+
+``` shell
+$ poly rust build
+You want to Build with Rust? Let me try to help you with that
+cargo build
+```
+
+
+### Possible improvements
+
+* add `package_install` command
+* add `file_extension` command
+* idk, actually do something besides print info, e.g:
+https://crates.io/crates/open
---
+++ src/main.rs
@@ -0,0 +1,154 @@
+use std::collections::HashMap;
+use clap::{Parser, ValueEnum};
+use lazy_static::lazy_static;
+
+struct Resources {
+    build: &'static str,
+    run: &'static str,
+    docs: &'static str,
+    test: &'static str,
+    repl: Option<&'static str>,
+}
+
+lazy_static! {
+    static ref M: HashMap<Lang, Resources> = {
+        let mut map = HashMap::new();
+        map.insert(Lang::C, Resources {
+            build: "clang",
+            run: "uhh, run the binary",
+            docs: "https://en.cppreference.com/w/c",
+            test: "good luck",
+            repl: None,
+        });
+        map.insert(Lang::CPP, Resources {
+            build: "clang++",
+            run: "uhh, run the binary",
+            docs: "https://en.cppreference.com/w/cpp",
+            test: "good luck",
+            repl: None,
+        });
+        map.insert(Lang::Zig, Resources {
+            build: "zig build",
+            run: "zig run",
+            docs: "https://ziglang.org/documentation/master/std/#A;std",
+            test: "zig test",
+            repl: None,
+        });
+        map.insert(Lang::Scheme, Resources {
+            build: "see repl",
+            run: "see repl",
+            docs: "https://www.gnu.org/software/guile/manual/guile.html#API-Reference",
+            test: "see repl",
+            repl: Some("guile"),
+        });
+        map.insert(Lang::CommonLisp, Resources {
+            build: "see repl",
+            run: "see repl",
+            docs: "https://www.lispworks.com/documentation/lw51/CLHS/Front/X_AllSym.htm",
+            test: "see repl",
+            repl: Some("sbcl"),
+        });
+        map.insert(Lang::Racket, Resources {
+            build: "see repl",
+            run: "see repl",
+            docs: "https://docs.racket-lang.org/reference/index.html",
+            test: "see repl",
+            repl: Some("racket"),
+        });
+        map.insert(Lang::Sml, Resources {
+            build: "- CM.make()",
+            run: "load files into repl using `use \"myfile.sml\"`",
+            docs: "https://smlfamily.github.io/Basis/overview.html",
+            test: "good luck",
+            repl: Some("sml"),
+        });
+        map.insert(Lang::Haskell, Resources {
+            build: "stack build",
+            run: "stack run",
+            docs: "https://hoogle.haskell.org/",
+            test: "stack test",
+            repl: Some("stack ghci"),
+        });
+        map.insert(Lang::Ocaml, Resources {
+            build: "dune build",
+            run: "dune exec <project_name>",
+            // https://v2.ocaml.org/releases/5.1/api/index.html
+            docs: "dmmulroy",
+            test: "dune test",
+            repl: Some("utop"),
+        });
+        map.insert(Lang::Rust, Resources {
+            build: "cargo build",
+            run: "cargo run",
+            docs: "https://doc.rust-lang.org/stable/std/",
+            test: "cargo test",
+            repl: Some("cargo install irust && irust"),
+        });
+        map.insert(Lang::Clojure, Resources {
+            build: "lein build",
+            run: "lein run",
+            docs: "https://clojuredocs.org",
+            test: "lein test",
+            repl: Some("lein repl"),
+        });
+        map
+    };
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, ValueEnum)]
+enum Lang {
+    C,
+    CPP,
+    Zig,
+    Scheme,
+    CommonLisp,
+    Racket,
+    Sml,
+    Haskell,
+    Ocaml,
+    Coq,
+    Agda,
+    Idris,
+    Rust,
+    Clojure,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Command {
+    Build,
+    Run,
+    Docs,
+    Test,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    lang: Lang,
+    command: Command,
+}
+
+impl Command {
+    fn print_resource(&self, r: &Resources) {
+        let res = match self {
+            Self::Build => r.build,
+            Self::Run => r.run,
+            Self::Docs => r.docs,
+            Self::Test => r.test,
+        };
+        println!("{}", res);
+    }
+}
+
+// return result from main
+fn main() {
+    let args = Args::parse();
+
+    println!("You want to {:?} with {:?}? Let me try to help you with that", args.command, args.lang);
+
+    match M.get(&args.lang) {
+        Some(r) => args.command.print_resource(r),
+        None => todo!("Crap, I'm sorry I didn't do this yet for {:?}", args.lang),
+    }
+}
